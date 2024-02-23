49e8fd4328770614c8cd8913e095f5c7fc281c5a
Arthur99Silva <arthurassilva99@gmail.com>
2024-02-22 22:57:26 -0300

    Inseto

--- Assets/Scriptable Objects/Patrono/Scripts/NamePatrono.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewTextAsset", menuName = "Assets/Scriptable Objects/Patrono/Name", order = 1)]
-public class NamePatrono : ScriptableObject
-{
-    [TextArea(15, 20)]
-    public string textContent;
-}
